cb623e4251e35205a7bc95f2b1fecc6913a021ad
rep-demo <aashishmijar44@gmail.com>
2025-05-04 19:05:01 +0545

    changed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: "./my-portfolio/",
+    base: "./",
     build: {
         outDir: 'dist',
         emptyOutDir: true
